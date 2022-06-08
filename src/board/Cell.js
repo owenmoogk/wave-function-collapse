7890048de0fe1e82f28dce6cab230fe57ad33b7a
Owen Moogk <58040528+owenmoogk@users.noreply.github.com>
2022-06-07 20:47:20 -0400

    added some small animations

--- src/board/Cell.js
+++ src/board/Cell.js
@@ -6,10 +6,20 @@ export default function Cell(props) {
   return (
     <div className='cell' row={row} column={column}>
       {Array.isArray(props.possibilities) ?
-
-        props.possibilities.map((option) => {
-          return (
-            <span className="possibility" onClick={() => props.cellClicked(row, column, option)}>{option}</span>
+        [1,2,3,4,5,6,7,8,9].map((option) => {
+          return(
+            <span className="possibility" onClick={() => {
+              if (!props.locked && props.possibilities.includes(option)){
+                props.cellClicked(row, column, option)
+              }
+            }} style={{
+              transition: 'opacity 2s, transform 1s',
+              opacity: props.possibilities.includes(option) ? '100%' : "0%",
+              color: props.possibilities.includes(option) ? '' : "blue",
+              fontWeight: props.possibilities.includes(option) ? '' : "bold",
+              transform: props.possibilities.includes(option) ? '' : "scale(2)",
+              zIndex: props.possibilities.includes(option) ? '' : '-1'
+            }}>{option}</span>
           )
         })
         : <span className="finalNumber">{props.possibilities}</span>
